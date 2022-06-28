41c075da703fc9f44b87ca2190d3253f3d045abe
Cory Forsyth <cory.forsyth@addepar.com>
2022-06-28 13:20:46 +0200

    set 1 challenge 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use cryptopals_rs::hex_2_base64;
+
 fn main() {
-    println!("Hello, world!");
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    println!(
+        "set 1 challenge 1: hex {} -> base64 {}",
+        hex,
+        hex_2_base64(hex)
+    );
 }
