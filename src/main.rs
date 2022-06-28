f4948d30e7bfc53df2b969e8986ea6a30b228dd3
Cory Forsyth <cory.forsyth@addepar.com>
2022-06-28 14:08:16 +0200

    set 1 challenge 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use cryptopals_rs::hex_2_base64;
+use cryptopals_rs::{set_1_challenge_1, set_1_challenge_2};
 
 fn main() {
-    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    println!(
-        "set 1 challenge 1: hex {} -> base64 {}",
-        hex,
-        hex_2_base64(hex)
-    );
+    set_1_challenge_1();
+    set_1_challenge_2();
 }
